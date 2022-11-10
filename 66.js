0afac9e0faa179782535cdfa005948805c05c779
gototherain <go.to.the.rain@gmail.com>
2022-11-10 19:41:47 +0600

    66

---
+++ 66.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }, 
+    {
+      "artist": "Metallica",
+      "title": "…And Justice for All ",
+      "release_year": 1988,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": false
+    }
+  ];
